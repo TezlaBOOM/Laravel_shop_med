cb74d18bb11d41d409e4dd31f1bddc03642888b7
Witold Michalak <Witold3152000@wp.pl>
2023-07-03 23:57:46 +0200

    admin -> edit profile

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -138,13 +138,13 @@
         <div class="d-sm-none d-lg-inline-block">Hi,</div></a>
         <div class="dropdown-menu dropdown-menu-right">
           <div class="dropdown-title">Logged in 5 min ago</div>
-          <a href="{{route('profile.edit')}}"class="dropdown-item has-icon">
+          <a href="{{route('admin.profile')}}"class="dropdown-item has-icon">
             <i class="far fa-user"></i> Profile
           </a>
           <a href="features-activities.html" class="dropdown-item has-icon">
             <i class="fas fa-bolt"></i> Activities
           </a>
-          <a href="features-settings.html" class="dropdown-item has-icon">
+          <a href="{{route('profile.edit')}}" class="dropdown-item has-icon">
             <i class="fas fa-cog"></i> Settings
           </a>
 
