a08f30814ebb5b3fbbfd628c47089e6d60d35ed7
Witold Michalak <Witold3152000@wp.pl>
2023-07-07 23:21:38 +0200

    fix and linking home page to login page

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('username')->nullable();
             $table->text('image')->nullable();
-            $table->string('nip')->unique();
+            $table->string('nip')->nullable();
             $table->string('phone')->nullable();
             $table->string('email')->unique();
             $table->enum('role',['admin','vendor','user'])->default('user');
