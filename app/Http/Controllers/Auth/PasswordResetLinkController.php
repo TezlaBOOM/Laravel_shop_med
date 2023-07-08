d94588242a0368fa3a03b313febe6d7eb8574a33
Witold Michalak <Witold3152000@wp.pl>
2023-07-08 16:44:47 +0200

    add frontend to reset password

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -36,6 +36,8 @@ class PasswordResetLinkController extends Controller
             $request->only('email')
         );
 
+        toastr('Link do zresetowaia hasła został wysłany na twojego maila');
+
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
                     : back()->withInput($request->only('email'))
