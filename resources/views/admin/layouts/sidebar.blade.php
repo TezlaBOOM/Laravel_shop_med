1b326a5d2f67bf907821ebc71a6ebf46802f7dab
Witold Michalak <Witold3152000@wp.pl>
2023-07-18 23:56:54 +0200

    (sidebar) item active depending on admin page

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -15,19 +15,26 @@
         </li>
         <li class="menu-header">Starter</li>
 
-        <li class="dropdown">
+        <li class="dropdown {{ setActive([
+          'admin.category.*',
+          'admin.sub-category.*',
+          'admin.child-category.*'
+
+        ])}}">
           <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Zarządzanie Kategoriami</span></a>
           <ul class="dropdown-menu">
-            <li><a class="nav-link" href="{{route('admin.category.index')}}">Kategorie</a></li>
-            <li><a class="nav-link" href="{{route('admin.sub-category.index')}}">Pod Kategorie</a></li>
-            <li><a class="nav-link" href="{{route('admin.child-category.index')}}">Pod pod Kategorie</a></li>
+            <li class="{{setActive(['admin.category.*'])}}"><a class="nav-link" href="{{route('admin.category.index')}}">Kategorie</a></li>
+            <li class="{{setActive(['admin.sub-category.*'])}}"><a class="nav-link" href="{{route('admin.sub-category.index')}}">Pod Kategorie</a></li>
+            <li class="{{setActive(['admin.child-category.*'])}}"><a class="nav-link" href="{{route('admin.child-category.index')}}">Pod pod Kategorie</a></li>
           </ul>
         </li>
 
-        <li class="dropdown">
+        <li class="dropdown {{setActive([
+          'admin.slider.*'
+        ])}}">
           <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Zarządzanie Stroną</span></a>
           <ul class="dropdown-menu">
-            <li><a class="nav-link" href="{{route('admin.slider.index')}}">Slider</a></li>
+            <li class="{{setActive(['admin.slider.*'])}}""><a class="nav-link" href="{{route('admin.slider.index')}}">Slider</a></li>
           </ul>
         </li>
         {{-- <li class="dropdown">
