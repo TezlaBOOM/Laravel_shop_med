81f3a957adb78965ce2d7a4b94a061fe53dfedcd
Witold Michalak <Witold3152000@wp.pl>
2023-10-23 00:27:22 +0200

    update newsletter mail

--- resources/views/mail/newsletter.blade.php
+++ resources/views/mail/newsletter.blade.php
@@ -1,192 +1,114 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Nasz Newsletter</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+
+    <style>
+        /* Styl ogólny */
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            padding: 0;
+            margin: 0 auto;
+            
+        }
+
+        /* Nagłówek */
+        header {
+            background-color: #3498db;
+            color: #fff;
+            text-align: center;
+            padding: 20px;
+          
+        }
+
+        /* Menu */
+        .menu-table {
+            background-color: #3b9209;
+            text-align: center;
+            max-width: 600px;
+            margin: 0 auto;
+
+            
+        }
+
+        .menu-table td {
+            padding: 10px;
+            width: 200px
+        }
+
+        .menu-table a {
+            color: #fff;
+            text-decoration: none;
+        }
+
+        /* Treść newslettera */
+        .newsletter-content {
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        /* Linki */
+        a {
+            color: #fff;
+            text-decoration: none;
+        }
+
+        /* Stopka */
+        footer {
+            background-color: #3498db;
+            color: #fff;
+            text-align: center;
+            padding: 20px;
+            margin: 0 auto;
+        }
+
+        /* Ikony mediów społecznościowych */
+        .social-icons {
+            margin-top: 10px;
+        }
+
+        .social-icons a {
+            color: #fff;
+            font-size: 20px;
+            margin: 0 10px;
+        }
+    </style>
 </head>
-<body style="background:#eeeeee; margin-top:0; margin-bottom:0; padding-top:0; padding-bottom:0;">
-<!-- WRAPPER-->
-<table align="center" border="0" cellpadding="0" cellspacing="0" width="640" style="border:none; display:block; width:640px;">
-    <!-- TOP-->
-    <tr>
-        <td height="10" style="height:10px;"></td>
-    </tr>
-    <tr>
-
-    </tr>
-    <tr>
-        <td bgcolor="#ffffff" width="640" style="-webkit-border-top-left-radius: 10px;-webkit-border-top-right-radius: 10px;-moz-border-radius-topleft: 10px;-moz-border-radius-topright: 10px;border-top-left-radius: 10px;border-top-right-radius: 10px; width:640px;">
-            <table align="center" border="0" cellpadding="0" cellspacing="0" width="640" style="border:none; display:block; webkit-border-top-left-radius: 10px;-webkit-border-top-right-radius: 10px;-moz-border-radius-topleft: 10px;-moz-border-radius-topright: 10px;border-top-left-radius: 10px;border-top-right-radius: 10px; width:640px;">
-                <tr>
-                    <td height="18" style="height:18px;"></td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <tr>
-        <td bgcolor="#ffffff" width="1200px" style="width:640px;">
-            <table align="center" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="640" style="border:none; display:block; width:1200px;">
-                <tr>
-                    <td class="rwd-600" align="center" width="105" valign="top" style="background-color:transparent;">
-                        <table valign="top" style="width:auto;max-width:105px;border-spacing:0;border-collapse: collapse;mso-cellspacing: 0;mso-padding-alt: 0 0 0 0;" align="center" width="105" cellspacing="0" cellpadding="0" border="0">
-                          <tbody>
-                            <tr width="100%" style="width: 100%">
-                              <td style="width: 105px;" align="center" width="105" valign="top">
-                                <p style="margin:0;padding:0;width:100%;text-align:center;"><a href="https://nowy.meringer.pl/kategoria/strefa-niskich-cen" style="text-decoration:none"><span style="font-weight:bold;font-family:Helvetica,Arial,sans-serif;font-size:16px;color:#f1faff;background-color:#00578e;line-height:1.4;"> Sklep</span></a></p>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </td>
-                      <td class="rwd-600" align="center" width="105" valign="top" style="background-color:transparent;">
-                        <table valign="top" style="width:auto;max-width:105px;border-spacing:0;border-collapse: collapse;mso-cellspacing: 0;mso-padding-alt: 0 0 0 0;" align="center" width="105" cellspacing="0" cellpadding="0" border="0">
-                          <tbody>
-                            <tr width="100%" style="width: 100%">
-                              <td style="width: 105px;" align="center" width="105" valign="top">
-                                <p style="margin:0;padding:0;width:100%;text-align:center;"><a href="https://nowy.meringer.pl/kategoria/strefa-niskich-cen" style="text-decoration:none"><span style="font-weight:bold;font-family:Helvetica,Arial,sans-serif;font-size:16px;color:#f1faff;background-color:#00578e;line-height:1.4;"> Oferta</span></a></p>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </td>
-                      <td class="rwd-600" align="center" width="105" valign="top" style="background-color:transparent;">
-                        <table valign="top" style="width:auto;max-width:105px;border-spacing:0;border-collapse: collapse;mso-cellspacing: 0;mso-padding-alt: 0 0 0 0;" align="center" width="105" cellspacing="0" cellpadding="0" border="0">
-                          <tbody>
-                            <tr width="100%" style="width: 100%">
-                              <td style="width: 105px;" align="center" width="105" valign="top">
-                                <p style="margin:0;padding:0;width:100%;text-align:center;"><a href="https://nowy.meringer.pl/kategoria/strefa-niskich-cen" style="text-decoration:none"><span style="font-weight:bold;font-family:Helvetica,Arial,sans-serif;font-size:16px;color:#f1faff;background-color:#00578e;line-height:1.4;"> Kontakt</span></a></p>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <!-- END OF TOP-->
-    <!-- CONTENT-->
-    <tr>
-        <td bgcolor="#acd037" width="640" style="width:640px;">
-            <table bgcolor="#acd037" align="center" border="0" cellpadding="0" cellspacing="0" width="640" style="border:none; display:block; width:640px;">
-                <tr>
-                    <td align="center" width="640" style="color:#707177; font-family:Arial, Helvetica, sans-serif; font-size:16px; width:640px;">
-                        <a href="" target="_blank" title="{{$subject}}" style="text-decoration:none;">
-                            <img src="{{$image}}" alt="{{$alttext}}" width="640" height="360" style="border:none; color:#000000; display:block; font-size:16px; text-align:left;" />
-                        </a>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <tr>
-        <td bgcolor="#ffffff" width="640" style="width:640px;">
-            <table bgcolor="#ffffff" align="center" border="0" cellpadding="0" cellspacing="0" width="640" style="display:block; border:none; width:640px;">
-                <tr>
-                    <td width="30" style="width:30px;"></td>
-                    <td bgcolor="#ffffff" width="580" style="width:580px;">
-
-
-
-                        {{$messageContent}}
-
-
-
-                    </td>
-                    <td width="30" style="width:30px;"></td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <tr>
-        <td bgcolor="#ffffff" width="640" style="-webkit-border-bottom-right-radius: 10px;-webkit-border-bottom-left-radius: 10px;-moz-border-radius-bottomright: 10px;-moz-border-radius-bottomleft: 10px;border-bottom-right-radius: 10px;border-bottom-left-radius: 10px; width:640px;">
-            <table bgcolor="#ffffff" align="center" border="0" cellpadding="0" cellspacing="0" width="640" style="border:none; display:block; width:640px;">
-                <tr>
-                    <td width="433" valign="top" style="color:#707177; font-family:Arial, Helvetica, sans-serif; font-size:14px; text-align:left; width:433px;">
-                        <table width="100%" border="0" cellpadding="0" cellspacing="0" style="border-spacing:0;border-collapse: collapse;mso-cellspacing: 0;mso-padding-alt: 0 0 0 0;">
-                            <tr>
-                              <td width="250" valign="top" style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;font-size: 0;" align="center">
-                                <div style="width:100%;max-width:49px;display:inline-block;vertical-align:top;">
-                                  <table width="49" border="0" cellpadding="0" cellspacing="0" style="width:49px;max-width:49px;border-spacing:0;border-collapse: collapse;mso-cellspacing: 0;mso-padding-alt: 0 0 0 0;" align="left">
-                                    <tbody>
-                                      <tr>
-                                        <td align="center" width="49" style="width:49px;max-width:49px;" valign="top"> <a href="https://www.facebook.com/meringerpl" style="font-size: 10px; width:49px;max-width:49px;"> <img width="49" src="" alt="Follow on facebook" style="width: 49px; max-width: 49px; border: none; display: block;" /> </a> </td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                                </div><!--[if (gte mso 9)|(IE)]></td><td valign="top" align="align" width="49"><![endif]-->
-                                <div style="width:100%;max-width:49px;display:inline-block;vertical-align:top;">
-                                  <table width="49" border="0" cellpadding="0" cellspacing="0" style="width:49px;max-width:49px;border-spacing:0;border-collapse: collapse;mso-cellspacing: 0;mso-padding-alt: 0 0 0 0;" align="left">
-                                    <tbody>
-                                      <tr>
-                                        <td align="center" width="49" style="width:49px;max-width:49px;" valign="top"> <a href="https://www.linkedin.com/company/meringer-sp-z-o-o-" style="font-size: 10px; width:49px;max-width:49px;"> <img width="49" src="" alt="Follow on linkedin" style="width: 49px; max-width: 49px; border: none; display: block;" /> </a> </td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                                </div><!--[if (gte mso 9)|(IE)]></td><td valign="top" align="align" width="49"><![endif]-->
-                                <div style="width:100%;max-width:49px;display:inline-block;vertical-align:top;">
-                                  <table width="49" border="0" cellpadding="0" cellspacing="0" style="width:49px;max-width:49px;border-spacing:0;border-collapse: collapse;mso-cellspacing: 0;mso-padding-alt: 0 0 0 0;" align="left">
-                                    <tbody>
-                                      <tr>
-                                        <td align="center" width="49" style="width:49px;max-width:49px;" valign="top"> <a href="https://www.youtube.com/channel/UCpy1Wrgg9jSMmeihk8CVZSw" style="font-size: 10px; width:49px;max-width:49px;"> <img width="49" src="" alt="Follow on youtube" style="width: 49px; max-width: 49px; border: none; display: block;" /> </a> </td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                                </div><!--[if (gte mso 9)|(IE)]></td><td valign="top" align="align" width="49"><![endif]-->
-                                <div style="width:100%;max-width:49px;display:inline-block;vertical-align:top;">
-                                  <table width="49" border="0" cellpadding="0" cellspacing="0" style="width:49px;max-width:49px;border-spacing:0;border-collapse: collapse;mso-cellspacing: 0;mso-padding-alt: 0 0 0 0;" align="left">
-                                    <tbody>
-                                      <tr>
-                                        <td align="center" width="49" style="width:49px;max-width:49px;" valign="top"> <a href="https://www.instagram.com/meringerpl/" style="font-size: 10px; width:49px;max-width:49px;"> <img width="49" src="" alt="Follow on istagram" style="width: 49px; max-width: 49px; border: none; display: block;" /> </a> </td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                                </div><!--[if (gte mso 9)|(IE)]></td><td valign="top" align="align" width="49"><![endif]-->
-                                <div style="width:100%;max-width:49px;display:inline-block;vertical-align:top;">
-                                  <table width="49" border="0" cellpadding="0" cellspacing="0" style="width:49px;max-width:49px;border-spacing:0;border-collapse: collapse;mso-cellspacing: 0;mso-padding-alt: 0 0 0 0;" align="left">
-                                    <tbody>
-                                      <tr>
-                                        <td align="center" width="49" style="width:49px;max-width:49px;" valign="top"> <a href="https://www.tiktok.com/@meringerpl" style="font-size: 10px; width:49px;max-width:49px;"> <img width="49" src="" alt="Follow on tiktok" style="width: 49px; max-width: 49px; border: none; display: block;" /> </a> </td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                                </div>
-                              </td>
-                            </tr>
-                          </table>
-                    </td>
-                    <td width="30" style="width:30px;"></td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <!-- END OF CONTENT-->
-    <!-- FOOTER -->
-    <tr>
-        <td bgcolor="#eeeeee" width="640" style="width:640px;">
-            <table align="center" border="0" cellpadding="0" cellspacing="0" style="border:none; display:block; width:640px;" width="640">
-                <tr>
-                    <td width="30" style="width:30px;"></td>
-                    <td width="580" style="width:580px;">
-                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="580" style="border:none; width:580px;">
-
-
-
-
-
-
-
-
-                        </table>
-                    </td>
-                    <td width="30" style="width:30px;"></td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <!-- END OF FOOTER 1-->
-    <!-- FOOTER 2 -->
-    <!-- END OF FOOTER 2-->
-</table>
-<!-- END OF WRAPPER-->
+<body>
+    <header>
+        <h1>Witaj w Naszym Newsletterze</h1>
+    </header>
+    <div class="menu">
+        <table class="menu-table">
+            <tr>
+                <td><a href="#sklep">Sklep</a></td>
+                <td><a href="#oferta">Oferta</a></td>
+                <td><a href="#kontakt">Kontakt</a></td>
+            </tr>
+        </table>
+    </div>
+    <div class="newsletter-content">
+        <h2>Nowości i Aktualności</h2>
+        <p>Witaj, drogi Klient! Oto najnowsze informacje i aktualności z naszej firmy:</p>
+        <p><img src="{{$image}}" alt="{{$alttext}}" style="max-width: 100%;"></p>
+        {{$messageContent}}
+        <p>Śledź nas na mediach społecznościowych, aby być na bieżąco!</p>
+    </div>
+    <footer>
+        &copy; 2023 Nasza Firma | <a href="mailto:info@nasza-firma.com">info@nasza-firma.com</a>
+        <div class="social-icons">
+            <a href="https://www.facebook.com/nasza-firma"><i class="fab fa-facebook"></i></a>
+            <a href="https://www.instagram.com/nasza-firma"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.youtube.com/user/nasza-firma"><i class="fab fa-youtube"></i></a>
+            <a href="https://www.linkedin.com/company/nasza-firma"><i class="fab fa-linkedin"></i></a>
+            <a href="https://twitter.com/nasza-firma"><i class="fab fa-twitter"></i></a>
+        </div>
+    </footer>
 </body>
 </html>
